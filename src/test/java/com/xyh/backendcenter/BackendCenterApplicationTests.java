b4d6fb2be4f757fe45dd1188e62181eb15d3ec82
juzi <2512936694@qq.com>
2024-01-06 09:16:22 +0800

    补充

---
+++ src/test/java/com/xyh/backendcenter/BackendCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xyh.backendcenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendCenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
